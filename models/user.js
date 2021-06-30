b8e5c372cb9ec9847bacbd83ee091fe6b7cffb46
Craig Connell <quietutrash@icloud.com>
2021-06-30 14:45:47 -0700

    "Passport JWT"

--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema({
-    admin: {
-        type: Boolean,
-        default: false
-    }
+  admin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema);
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", userSchema);
